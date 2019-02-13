a160a3d436f6808c9dedb29e49065b39e145b25a
sheenams <sheenams@uw.edu>
2019-02-13 15:34:16 -0800

    clean up refseq/refgene naming

--- ngs_capture_qc/subcommands/refgene_to_bed.py
+++ ngs_capture_qc/subcommands/refgene_to_bed.py
@@ -6,7 +6,7 @@ import os
 import sys 
  
 def build_parser(parser):
-    parser.add_argument('refseq', help='UCSC table browser download')
+    parser.add_argument('refgene', help='UCSC table browser download')
     parser.add_argument('output', help='Output file', default = sys.stdout)
 
  
@@ -21,7 +21,7 @@ def bed_key(d):
 
 def action(args):
     genes = {}
-    for line in open(args.refseq, 'r'):
+    for line in open(args.refgene, 'r'):
         ls = line.strip().split('\t')
         if len(ls)<16:
             sys.exit("File expected to have the following columns: 'bin', 'name', 'chrom', 'strand', 'txStart', 'txEnd', 'cdsStart', 'cdsEnd', 'exonCount', 'exonStarts', 'exonEnds', 'score', 'name2', 'cdsStartStat', 'cdsEndStat', 'exonFrames'")
@@ -33,22 +33,22 @@ def action(args):
         relstarts = get_string_list(starts) #[start - int(ls[4]) for start in starts])
         relends = get_string_list(stops) #[start - int(ls[4]) for start in starts])
         # For format see http://genome.ucsc.edu/FAQ/FAQformat.html#format1
-        features = ['chrom','chromStart','chromEnd','name', 'refseq','exonCount','exonSizes','exonStarts','exonEnds']
+        features = ['chrom','chromStart','chromEnd','name', 'refgene','exonCount','exonSizes','exonStarts','exonEnds']
         gene_entry = dict([('chrom', ls[2].strip('chr')),
                            ('chromStart', ls[4]),
                            ('chromEnd', ls[5]),
                            ('name', ls[12]),
-                           ('refseq', ls[1]),
+                           ('refgene', ls[1]),
                            ('exonCount', ls[8]),
                            ('exonSizes', lengths),
                            ('exonStarts', relstarts),
                            ('exonEnds', relends)])
-        refseq = ls[1]
+        refgene = ls[1]
         
-        # Ensure that each refseq is only in the table once
-        if (refseq not in genes 
-            and 'NM' in refseq):
-            genes[refseq] = gene_entry
+        # Ensure that each refgene is only in the table once
+        if (refgene not in genes 
+            and 'NM' in refgene):
+            genes[refgene] = gene_entry
 
     output=open(args.output,'w')
     for gene in sorted(genes.values(), key=bed_key):
