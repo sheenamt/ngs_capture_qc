d30cd705b3546d7ea82af7da7f4abda6ec34768d
sheenams <sheenams@uw.edu>
2019-02-14 15:26:06 -0800

    update tests for create_files

--- tests/test_createfiles.py
+++ tests/test_createfiles.py
@@ -13,6 +13,7 @@ from __init__ import TestBase
 import __init__ as config
 log = logging.getLogger(__name__)
 
+testfiles=config.datadir
 
 class TestCreateFiles(TestBase):
     """
@@ -22,7 +23,23 @@ class TestCreateFiles(TestBase):
 
     def setUp(self):
         self.outdir = self.mkoutdir()
+        self.output_basename=os.path.join(testfiles,'testoutput')
         self.data=[['1', '3407032', '3407152','275744_14961323_MEGF6_chr1:3407091-3407153_1', '+'],['1', '3407036', '3407158','chr1:3407091-3407153_1', '-']]
+        self.bedtools_image='/mnt/disk2/com/container-images/bedtools-2.26.img'
+        self.bedtools='singularity exec --bind {} --pwd {} {}'.format(os.getcwd(), os.getcwd(), self.bedtools_image)
+        self.refgene_bed = os.path.join(testfiles, 'expected.refGene.bed')
+        self.probe_file=os.path.join(testfiles,'test.probes')
+        self.probes_df = None
+        self.probes_temp = None
+        self.setupTempProbes()
+
+    def setupTempProbes(self):
+        pf=open(self.probe_file,'r')
+        probes = pd.read_csv(pf, delimiter='\t')
+        self.probes_df = create_files.check_probe_format(probes)
+        self.probes_temp=os.path.join(self.outdir,'testoutput-TEMP.probes')
+        self.probes_df.to_csv(self.probes_temp, columns=['chrom','start','stop'],header=False,sep='\t', index=False)
+        pf.close()
 
     def break_data(self,data,string_to_break,index):
         self.new_data=[]
@@ -30,7 +47,6 @@ class TestCreateFiles(TestBase):
             x[index]=string_to_break
             self.new_data.append(x)
         return self.new_data
-
 
     def testCheckFormat1(self):
         """Check that there is no header"""
@@ -44,7 +60,6 @@ class TestCreateFiles(TestBase):
 
     def testCheckFormat3(self):
         """Check that the start and stop columns are integers """
-
         start_probes=pd.DataFrame(data=self.break_data(self.data,'bad',1))
         self.assertRaises(ValueError,create_files.check_probe_format,start_probes)
 
@@ -53,14 +68,49 @@ class TestCreateFiles(TestBase):
 
     def testCheckFormat4(self):
         """Check that there is a strand column """
-
         strand_probes=pd.DataFrame(data=self.break_data(self.data,'bad',4))
         self.assertRaises(ValueError, create_files.check_probe_format,strand_probes)
 
     def testCheckFormat5(self):
         """Check that there is a string of some sort in the annotation column """
-
         annot_probes=pd.DataFrame(data=self.break_data(self.data,'4',4))
         self.assertRaises(ValueError, create_files.check_probe_format, annot_probes)
 
+    def testCreatePicardBed(self):
+        """Use doc/PicardHeader and probe file to create a file 
+        in the format required by picard
+        """
+        expected_output=os.path.join(testfiles,'expected.Picard.bed')
+        testing_output=os.path.join(self.outdir,'testoutput')
+        create_files.create_picard_bed(self.probes_df, testing_output)
+        self.assertTrue(filecmp.cmp(expected_output, os.path.join(self.outdir, 'testoutput.Picard.bed')))
             
+
+    def testWriteMerged_Bed(self):
+        expected_output=os.path.join(testfiles, 'expected-TEMP.bed')
+        testing_output=os.path.join(self.outdir,'testoutput.bed')
+        create_files.write_merged_bed(self.probes_temp, self.bedtools, testing_output)
+        self.assertTrue(filecmp.cmp(expected_output, testing_output))
+
+    def testWriteAnnotatedBed(self):
+        #Write merged, annotated bed file
+        expected_output=os.path.join(testfiles,'expected-ANNO.bed')
+        input_bed=os.path.join(testfiles, 'expected-TEMP.bed')
+        anno_bed=os.path.join(self.outdir,'testoutput.anno.bed')
+        create_files.write_annotated_bed(input_bed, self.bedtools, self.refgene_bed, anno_bed)
+        self.assertTrue(filecmp.cmp(expected_output, anno_bed))
+
+    def testCreateFiles(self):
+        #Test running of whole script
+        cmd=["/mnt/disk10/users/sheenams/ngs_capture_qc/cap_qc.py", "create_files", self.probe_file, "--refgene_bed", self.refgene_bed, "--outdir", self.outdir, "--bedtools", self.bedtools_image, "--picard", "--bed"]
+        subprocess.call(cmd)
+        
+        expected_picard=os.path.join(testfiles,'expected.Picard.bed')
+        expected_bed=os.path.join(testfiles,'expected-ANNO.bed')
+        testing_picard=os.path.join(self.outdir,'test.Picard.bed')
+        testing_bed=os.path.join(self.outdir,'test.anno.bed')
+        self.assertTrue(filecmp.cmp(expected_picard, testing_picard))
+        self.assertTrue(filecmp.cmp(expected_bed, testing_bed))
+  
+  
+
