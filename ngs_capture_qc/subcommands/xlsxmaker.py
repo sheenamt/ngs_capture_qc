a38fa9887c1a53cb8a142b962f2e727f0318c88f
sheenamt <sheena.todhunter@gmail.com>
2020-11-17 09:06:32 -0800

    add script to create excel workbook from assay summary files

---
+++ ngs_capture_qc/subcommands/xlsxmaker.py
@@ -0,0 +1,44 @@
+"""
+Create xlsx workbook from all output files
+
+usage:
+
+ capqc xlsmaker /path/to/summary/files
+
+"""
+import csv
+import re
+import os
+
+from xlsxwriter import Workbook
+
+def build_parser(parser):
+    parser.add_argument(
+        'infiles', action='append', nargs='+',
+        help='Input files')
+    parser.add_argument(
+        '-o', '--outfile',
+        help='Output file', required=True)
+
+def write_workbook(sheet_name, book, fname):
+    """
+    Write analysis file as sheet in workbook
+    """
+    sheet = book.add_worksheet(sheet_name)    
+    Reader = csv.reader(open(fname, 'rU'), delimiter='\t')
+    for rowx, row in enumerate(Reader):
+        for colx, value in enumerate(row):
+            sheet.write(rowx, colx, value)
+
+def action(args):
+
+    book = Workbook()
+    (infiles, ) = args.infiles
+    for fname in infiles:
+        (f_path, f_name) = os.path.split(fname)
+        (f_short_name, f_extension) = os.path.splitext(f_name)
+        print(fname)
+        write_workbook(f_short_name, book, fname)
+    book.filename=args.outfile
+
+    book.close()
